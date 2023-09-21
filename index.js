8f75be3d7421c9bd060e5869d4b836eca18a08ba
Muhammad Osama Noor <mosamanoor17@gmail.com>
2023-09-21 23:50:39 +0500

    main

---
+++ index.js
@@ -0,0 +1,16 @@
+gsap.to("#nav",{
+    backgroundColor: "#000",
+    duration:0.5,
+    height:"110px",
+    scrollTrigger:{
+            trigger:"#nav",
+            scroll:"body",
+            markers:true,
+            start:"top -10%",
+            end:"top -11%",
+            scrub:1
+
+    }
+}
+    
+)
